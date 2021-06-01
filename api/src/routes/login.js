2ab197ec94bf2d7aa11d50f3fbfb39ca86289bdd
Emanuel <bolzicoemanuel@gmail.com>
2021-06-01 12:43:58 -0300

    Back & front connected

--- api/src/routes/login.js
+++ api/src/routes/login.js
@@ -3,22 +3,22 @@ const bcrypt = require('bcrypt');
 const saltRounds = 10;
 
 async function authenticate(req, res){
-    let user= {
+    const user= {
         email: req.body.email,
         password: req.body.password
     }
-    console.log(user)
     try {
         let hash= bcrypt.hashSync(user.password, saltRounds)
-        let userToCompare= await User.findOne({email: user.email})
+        const userToCompare= await User.findOne({email: user.email})
+        if (!userToCompare) {return res.status(404).json({message: 'The username or password does not exist'})}
         if (bcrypt.compareSync(userToCompare.password, hash)){
             // Generar token y pasar
             res.json('Paso')
         } else {
-            res.status(404).json('The username or password does not exist')
+            res.status(404).json({message: 'The username or password does not exist'})
         }
     }
-    catch(error){console.log(error)}
+    catch(error){res.status(400).json({message: 'Bad body request'})}
 }
 
 module.exports= {
--- api/src/routes/user.js
+++ api/src/routes/user.js
@@ -13,9 +13,10 @@ async function userInfo(req, res){
     */
     try{
         let userData= await User.findOne({email: user.email, password: user.password})
+        if (!userData) {return res.status(404).json({message: 'The username or password does not exist'})}
         res.status(200).json(userData)
     }
-    catch(error){}
+    catch(error){res.status(400).json({message: 'Bad body request'})}
 }
 
 module.exports= {
